681e8aa237e9e90ad250bc105d03e3f9f3583007
Lukas Ostien <lukas.ostien@gmail.com>
2026-01-17 17:12:15 -0800

    Update enforceGeometry.jl

--- Subroutines/enforceGeometry.jl
+++ Subroutines/enforceGeometry.jl
@@ -1,5 +1,6 @@
 include("findBoundary.jl")
 function enforceGeometry(node,elem,R)
+    # After refining a circle mesh, ensure the boundary nodes are along the shape of a circle.
     # ensures boundary nodes are in the shape of a circle
     bdNode = findBoundary(elem);
     r = sqrt.(node[bdNode,1].^2+node[bdNode,2].^2);
